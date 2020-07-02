750b13478da44ae0ed178ae4443df5ed2c88796c
Kalyan Kumar Kancharla <48640903+Kalyan457@users.noreply.github.com>
2020-07-01 22:07:08 -0500

    Add files via upload

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -12,7 +12,8 @@ class HomePage extends Component {
 
     state={
         highlightedMenuId : 'menu_1',
-        postingNewPost: false
+        postingNewPost: false,
+        savePostState:[]
     }
 
     menuItemClickHandler = (event) => {
@@ -43,6 +44,37 @@ class HomePage extends Component {
         document.getElementById('editable').focus();
     } 
 
+    saveBtnHandler = (event) => {
+        event.preventDefault();
+        var selectedBtnId=event.target.id;
+        if(selectedBtnId !== undefined){
+            console.log(selectedBtnId);
+            var selectedBtnValue=event.target.value;
+            var control = document.getElementById(selectedBtnId);
+            var savePostStateCopy = this.state.savePostState.slice();
+            var btnText = control.innerText;
+            if(savePostStateCopy.includes(selectedBtnValue)){
+                console.log(btnText);
+                btnText=btnText.substring(0,btnText.length-3);
+                console.log(btnText);
+                control.innerText=btnText;
+                control.style.color="rgb(60, 154, 84)";
+                control.style.backgroundColor="white";
+                savePostStateCopy = savePostStateCopy.filter(item => item !== selectedBtnValue);
+            }
+            else{
+                btnText=btnText+"d"+ " "+'&#10003;';
+                console.log(btnText);
+                control.innerHTML=btnText;
+                control.style.color="rgb(60, 154, 84)";
+                control.style.backgroundColor="white";
+                savePostStateCopy.push(selectedBtnValue);
+            }
+            console.log(savePostStateCopy);
+            this.setState({savePostState:savePostStateCopy});
+        }
+    };
+
     render(){
         var modal=null;
         if(this.state.postingNewPost){
@@ -56,6 +88,8 @@ class HomePage extends Component {
                         <img 
                             src={LfrmMImg} 
                             alt="LfrmM Logo" 
+                            height="56"
+                            width="100"
                             style={{position:'absolute'}} />
                         <input className={classes.searchInput} type="text" name="searchInp" placeholder="Search..." />   
                         <a className={classes.logoutLink}>Logout</a> 
@@ -108,81 +142,38 @@ class HomePage extends Component {
                             {modal}
                         </div>
                         <div className={classes.userPosts}>
-                            <div className={classes.profileDetails}>
-                                <div className={classes.profilePicDiv}>
-                                    <img src={defaultProfilePic}  className={classes.profileImage}/>
-                                    <span style={{marginLeft:"10px",marginTop:"16px",display:'inline'}}>Profile Name</span>                                </div>
-                                <div className={classes.saveReportDiv}>
-                                    <button className={classes.reportPostBtn}>Report</button>
-                                    <button className={classes.reportPostBtn}>Save</button>
+                            <span style={{float:'right',color:'green',fontSize:'20px'}}>&#10003;</span>
+                            <button 
+                                className={classes.savePostBtn}
+                                value="1" 
+                                id="savePost_Btn"
+                                onClick={this.saveBtnHandler.bind(this)}>Save</button>
+                            <div className={classes.dropdown}>
+                                <button className={classes.reportPostBtn}>Report</button>
+                                <div className={classes.dropdownContent}>
+                                    <a href="#">
+                                        <h5 style={{paddingBottom:'-10px',marginBottom:'-20px',marginTop:'0px'}}>Block this post</h5>
+                                        <h6 style={{marginBottom:'-5px',marginTop:'17px',fontWeight:'lighter',color:'gray'}}>This post is offensive</h6>
+                                    </a>
+                                    <a href="#">
+                                        <h5 style={{paddingBottom:'-10px',marginBottom:'-20px',marginTop:'0px'}}>Irrelevant post</h5>
+                                        <h6 style={{marginBottom:'-5px',marginTop:'17px',fontWeight:'lighter',color:'gray'}}>Not relevant to this community</h6>
+                                    </a>
+                                    <a href="#">
+                                        <h5 style={{paddingBottom:'-10px',marginBottom:'-20px',marginTop:'0px'}}>Hide post</h5>
+                                        <h6 style={{marginBottom:'0px',marginTop:'17px',fontWeight:'lighter',color:'gray'}}>I don't want to see this post</h6>
+                                    </a>
                                 </div>
-                                <div className={classes.clear}></div>
                             </div>
-                            <div>
-                                <h4 style={{marginTop: "15px", marginBottom: "3px", textAlign: 'left', paddingLeft: '10px'}}>
-                                    <span className={classes.redFont}>#Mistake</span>
-                                </h4>
-                                <label style={{paddingLeft: '10px'}}>This is my Mistake</label>
-                            </div>
-                            <div>
-                                <h4 style={{marginTop: "15px", marginBottom: "3px", textAlign: 'left', paddingLeft: '10px'}}>
-                                    <span className={classes.greenFont}>#Learning</span>
-                                </h4>
-                                <label style={{paddingLeft: '10px'}}>This is my Learning</label>
-                            </div>
-                            <div className={classes.postReactionsCountDiv}>
-                                <span className={classes.postReactionsCount}>5 &#128161;</span>
-                                <span className={classes.postReactionsCount}>10 &#128079;</span>
-                                <span className={classes.postReactionsCount}>2 <FontAwesomeIcon icon={faShare} color="rgb(244,200,7)" size="xs" /></span>
-                                <span className={classes.postReactionsCount}>5 <FontAwesomeIcon icon={faCommentAlt} color="gray" size="xs" /></span>
-                            </div>
-                            <div className={classes.postReactionsDiv}>
-                                <button className={classes.postReactionsBtns}>&#128161; Learned</button>
-                                <button className={classes.postReactionsBtns}>&#x1f44f; Appreciate</button>
-                                <button className={classes.postReactionsBtns}><FontAwesomeIcon icon={faShare} color="rgb(244,200,7)" size="xs" /> Share</button>
-                                <button className={classes.postReactionsBtns}><FontAwesomeIcon icon={faCommentAlt} color="gray" size="xs" /> Comment</button>
-                            </div>
-                        </div>
-                        <div className={classes.userPosts}>
                             <div className={classes.profileDetails}>
-                                <img src={defaultProfilePic}  className={classes.profileImage}/>
-                                <span style={{marginLeft:"10px",marginTop:"16px"}}>Profile Name</span>
+                                    <img src={defaultProfilePic}  className={classes.profileImage}/>
+                                    <span style={{marginLeft:"10px",marginTop:"16px", display:'inline'}}>Profile Name</span>
                             </div>
                             <div>
                                 <h4 style={{marginTop: "15px", marginBottom: "3px", textAlign: 'left', paddingLeft: '10px'}}>
                                     <span className={classes.redFont}>#Mistake</span>
                                 </h4>
-                                <label style={{paddingLeft: '10px'}}>This is my Mistake</label>
-                            </div>
-                            <div>
-                                <h4 style={{marginTop: "15px", marginBottom: "3px", textAlign: 'left', paddingLeft: '10px'}}>
-                                    <span className={classes.greenFont}>#Learning</span>
-                                </h4>
-                                <label style={{paddingLeft: '10px'}}>This is my Learning</label>
-                            </div>
-                            <div className={classes.postReactionsCountDiv}>
-                                <span className={classes.postReactionsCount}>5 &#128161;</span>
-                                <span className={classes.postReactionsCount}>10 &#128079;</span>
-                                <span className={classes.postReactionsCount}>2 <FontAwesomeIcon icon={faShare} color="rgb(244,200,7)" size="xs" /></span>
-                                <span className={classes.postReactionsCount}>5 <FontAwesomeIcon icon={faCommentAlt} color="gray" size="xs" /></span>
-                            </div>
-                            <div className={classes.postReactionsDiv}>
-                                <button className={classes.postReactionsBtns}>&#128161; Learned</button>
-                                <button className={classes.postReactionsBtns}>&#x1f44f; Appreciate</button>
-                                <button className={classes.postReactionsBtns}><FontAwesomeIcon icon={faShare} color="rgb(244,200,7)" size="xs" /> Share</button>
-                                <button className={classes.postReactionsBtns}><FontAwesomeIcon icon={faCommentAlt} color="gray" size="xs" /> Comment</button>
-                            </div>
-                        </div>
-                        <div className={classes.userPosts}>
-                            <div className={classes.profileDetails}>
-                                <img src={defaultProfilePic}  className={classes.profileImage}/>
-                                <span style={{marginLeft:"10px",marginTop:"16px"}}>Profile Name</span>
-                            </div>
-                            <div>
-                                <h4 style={{marginTop: "15px", marginBottom: "3px", textAlign: 'left', paddingLeft: '10px'}}>
-                                    <span className={classes.redFont}>#Mistake</span>
-                                </h4>
-                                <label style={{paddingLeft: '10px'}}>This is my Mistake</label>
+                                <label style={{paddingLeft: '10px'}}>This is my Mistake. Hope you do not make it as I did, as it screwed me.</label>
                             </div>
                             <div>
                                 <h4 style={{marginTop: "15px", marginBottom: "3px", textAlign: 'left', paddingLeft: '10px'}}>
