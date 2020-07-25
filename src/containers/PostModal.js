75f4a9a376d5a17b5b8e9100c6ede237ef89417f
Kalyan Kumar Kancharla <48640903+Kalyan457@users.noreply.github.com>
2020-07-25 01:13:28 -0500

    changes to post modal

--- src/containers/PostModal.js
+++ src/containers/PostModal.js
@@ -1,11 +1,19 @@
 import React ,{ Component } from 'react'
 import classes from './PostModal.css';
 import Modal from '../components/UI/Modal';
+import imageUploadIcon from '../assets/images/imageUpload.png';
+import linkIcon from '../assets/images/linkIcon.png';
 
 class PostModal extends Component {
-     componentDidMount(){
-         document.getElementById('editable1').focus();
-     }   
+
+    state={
+        selectedFile:null
+    }
+
+    componentDidMount(){
+        document.getElementById('editable1').focus();
+        // this.fileSelector = this.buildFileSelector();
+    }   
     
     constructor(props) {
         super(props);
@@ -19,6 +27,18 @@ class PostModal extends Component {
     closeModalHandler = (event) => {
         event.stopPropagation();
         this.props.parentMethod();
+    }
+
+    fileSelectedHandler = (event) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            if(reader.readyState==2){
+                this.setState({
+                    selectedFile:reader.result
+                })
+            }
+        }
+        reader.readAsDataURL(event.target.files[0]);
     }
       
     render(){
@@ -35,17 +55,67 @@ class PostModal extends Component {
                         <h4 style={{marginTop: "15px", marginBottom: "3px", textAlign: 'left', paddingLeft: '10px'}}>
                             <span className={classes.redFont}>#Mistake</span>
                         </h4>
-                       <div className={classes.contentDiv} id="editable1" contentEditable = "true" placeholder="Express your mistake if any, else post your learning below." style={{paddingLeft: '10px'}}>
+                        <div 
+                            className={classes.contentDiv} 
+                            id="editable1" 
+                            contentEditable = "true" 
+                            placeholder="Express your mistake if any, else post your learning below." 
+                            style={{paddingLeft: '10px'}}>
+                            {this.state.selectedFile ? <img src={this.state.selectedFile} width="100px" height="100px"/> : null }
                         </div>
-                        <div className={classes.attachments}></div>
+                        <div className={classes.attachments}>
+                            <input 
+                                type="file" 
+                                style={{display:'none'}}
+                                onChange={this.fileSelectedHandler} 
+                                accept="image/*"
+                                ref={fileInput => this.fileInput = fileInput} />
+                            <img 
+                                src={imageUploadIcon} 
+                                alt="Upload Image" 
+                                height="20"
+                                width="30"
+                                onClick={()=>this.fileInput.click()}
+                                />
+                            <img 
+                            src={linkIcon} 
+                            alt="Upload Image" 
+                            height="20"
+                            width="30"
+                            />
+                        </div>
                     </div>
                     <div>
                         <h4 style={{marginTop: "15px", marginBottom: "3px", textAlign: 'left', paddingLeft: '10px'}}>
                             <span className={classes.greenFont}>#Learning</span>
                         </h4>
-                        <div className={classes.contentDiv}  contentEditable = "true" placeholder="Hurray... I learned this new thing and hope it helps y'all. " style={{paddingLeft: '10px'}}>
+                        <div 
+                            className={classes.contentDiv}  
+                            contentEditable = "true" 
+                            placeholder="Hurray... I learned this new thing and hope it helps y'all. " 
+                            style={{paddingLeft: '10px'}}>
                         </div>
-                        <div className={classes.attachments}></div>
+                        <div className={classes.attachments}>
+                            <input 
+                                type="file" 
+                                style={{display:'none'}}
+                                onChange={this.fileSelectedHandler} 
+                                accept="image/*"
+                                ref={fileInput => this.fileInput = fileInput} />
+                            <img 
+                                src={imageUploadIcon} 
+                                alt="Upload Image" 
+                                height="20"
+                                width="30"
+                                onClick={()=>this.fileInput.click()}
+                                />
+                            <img 
+                                src={linkIcon} 
+                                alt="Upload Image" 
+                                height="20"
+                                width="30"
+                                />
+                        </div>
                     </div>
                     <div>
                         <input type="checkbox" id="anonymous" name="anonymousChkBtn" value="Anonymous" style={{marginTop:"10px"}} />
